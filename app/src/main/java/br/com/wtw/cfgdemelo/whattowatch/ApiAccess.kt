96da13e62e4b7b1c9625f44810eb2d1a396fafda
Caio Melo <cfgdemelo@gmail.com>
2018-04-03 22:42:07 -0300

    Adding Picasso, retrieving movie image, adding Sofa icon, improving Kotlin Extension and adding InputTextField

--- app/src/main/java/br/com/wtw/cfgdemelo/whattowatch/ApiAccess.kt
+++ app/src/main/java/br/com/wtw/cfgdemelo/whattowatch/ApiAccess.kt
@@ -1,33 +1,32 @@
 package br.com.wtw.cfgdemelo.whattowatch
 
-import android.content.res.Resources
-import android.os.AsyncTask
+import android.net.Uri
 import info.movito.themoviedbapi.TmdbApi
 import info.movito.themoviedbapi.TmdbMovies
+import info.movito.themoviedbapi.model.MovieDb
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.doAsyncResult
 
 class ApiAccess(private val main: MainActivity) {
 
-    fun getMovieTitle() {
-        var movies: TmdbMovies? = null
-        var title: String? = null
-        doAsync {
-            val apiKey = "7f2323ddb4283932ca4972d14200c4f2"
-            movies = TmdbApi(apiKey).movies
-            title = movies?.getMovie(186734, "en")?.title
-            main.setMovieTitle(title)
-        }
-    }
-
-    fun getMovieTime() {
+    fun getMovieInfo(movieId: Int) {
         var movies: TmdbMovies?
+        var title: String?
         var time: Int?
+        var imgPath = "https://image.tmdb.org/t/p/original"
+        var moviesMap: MovieDb?
         doAsync {
             val apiKey = "7f2323ddb4283932ca4972d14200c4f2"
             movies = TmdbApi(apiKey).movies
-            time = movies?.getMovie(186734, "en")?.runtime
+            moviesMap = movies?.getMovie(movieId, "en")
+            title = moviesMap?.title
+            time = moviesMap?.runtime
+            imgPath += moviesMap?.posterPath
+            main.setMovieTitle(title)
             main.setMovieTime(time)
+            main.runOnUiThread {
+                main.setMovieImg(Uri.parse(imgPath))
+            }
         }
+
     }
 }
--- app/src/main/java/br/com/wtw/cfgdemelo/whattowatch/MainActivity.kt
+++ app/src/main/java/br/com/wtw/cfgdemelo/whattowatch/MainActivity.kt
@@ -1,20 +1,23 @@
 package br.com.wtw.cfgdemelo.whattowatch
 
 import android.content.res.Resources
+import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
 import android.widget.TextView
-import info.movito.themoviedbapi.model.MovieDb
+import com.squareup.picasso.Picasso
 
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.imageURI
 
 class MainActivity : AppCompatActivity() {
 
     private var imgMovie: ImageView? = null
     private var titleMovie: TextView? = null
     private var timeMovie: TextView? = null
+    private var inputMovie: TextInputEditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +27,13 @@ class MainActivity : AppCompatActivity() {
         imgMovie = findViewById(R.id.imgMovie)
         titleMovie = findViewById(R.id.titleMovie)
         timeMovie = findViewById(R.id.timeMovie)
+        inputMovie = findViewById(R.id.inputMovie)
+
         val api = ApiAccess(this)
 
         fab.setOnClickListener {
-            api.getMovieTitle()
-            api.getMovieTime()
+            val inputedMovie = inputMovie?.text
+            api.getMovieInfo(Integer.parseInt(inputedMovie.toString()))
         }
     }
 
@@ -40,6 +45,10 @@ class MainActivity : AppCompatActivity() {
         timeMovie?.text = movieTime?.swap()
     }
 
+    fun setMovieImg(movieImg: Uri) {
+        Picasso.get().load(movieImg).into(imgMovie)
+    }
+
     private fun Int.swap() : String {
         val hoursInt = this / 60
         val minutesLeft = this % 60
@@ -47,9 +56,18 @@ class MainActivity : AppCompatActivity() {
         val minutesMeter = if (minutesLeft > 1) getText(R.string.minutes) else getText(R.string.minute)
         val and = getText(R.string.and)
         val noHours = "$minutesLeft $minutesMeter"
-        val withHours = "$hoursInt $hoursMeter $and $minutesLeft $minutesMeter"
-        var time: String
-        if (hoursInt > 0) time = withHours else time = noHours
+        val hoursAndMinutes = "$hoursInt $hoursMeter $and $minutesLeft $minutesMeter"
+        val noMinutes = "$hoursInt $hoursMeter"
+        val time: String
+
+        if (hoursInt > 0 && minutesLeft > 0) {
+            time = hoursAndMinutes
+        } else if (minutesLeft > 0) {
+            time = noHours
+        } else {
+            time = noMinutes
+        }
+
         return time
     }
 }
